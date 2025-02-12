65ba7c9faa4b2494da00c3b8ffdd0f6434a2b192
Thant Zin Lin <tzlin@mit.com.mm>
2025-02-12 14:47:49 +0630

    edit with slug

--- src/app/categories/edit/[slug]/page.tsx
+++ src/app/categories/edit/[slug]/page.tsx
@@ -1,11 +1,18 @@
-import { useRouter } from "next/router";
+'use client'; 
+//import { useRouter } from "next/router";
+import { useParams } from 'next/navigation';
+
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import CategoryForm from "@/components/Category/CategoryForm";
 
 const EditCategoryPage = () => {
-  const router = useRouter();
-  const { slug } = router.query;
+  // const router = useRouter();
+  // const { slug } = router.query;
+  const params = useParams();
+  const slug = params.slug;
+
+
   if (!slug) {
     return <p>Loading...</p>; // Or show an error/loading state while the slug is being resolved
   }
